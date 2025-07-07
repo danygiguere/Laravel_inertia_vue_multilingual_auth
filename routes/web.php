f68b3c958ced1d53d08e9104bc05a73099d68446
Dany Giguere <danygiguere23@gmail.com>
2025-07-07 16:59:43 -0400

    u

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/'. App::getLocale());
+
+foreach (config('app.supported_locales') as $locale) {
+    Route::prefix($locale)->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        });
+    });
+}
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,39 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('/ redirects to /en', function () {
     $response = $this->get('/');
+    $response->assertStatus(302);
+    $response->assertRedirect('/en');
+});
+
+test('/en returns a successful response', function () {
+    $response = $this->get('/en');
 
     $response->assertStatus(200);
 });
+
+test('/fr returns a successful response', function () {
+    $response = $this->get('/fr');
+
+    $response->assertStatus(200);
+});
+
+test('/de returns a successful response', function () {
+    $response = $this->get('/de');
+
+    $response->assertStatus(404);
+});
+
+
+test('/en/dashboard returns a successful response', function () {
+    $response = $this->get('/en/dashboard');
+
+    $response->assertStatus(200);
+});
+
+test('/fr/dashboard returns a successful response', function () {
+    $response = $this->get('/en/dashboard');
+
+    $response->assertStatus(200);
+});
+
